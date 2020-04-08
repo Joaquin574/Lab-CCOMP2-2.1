fb40293fb9d0d7fc5b2d11b05ae85f0652b9f4b2
Joaquin Luque <joaquin.luque@ucsp.edu.pe>
2020-04-07 21:10:07 -0500

    CCOMP 2-2.1

---
+++ Lab-7-abril.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <cstring>
+
+
+int main()
+{
+    /*int i1, i2;
+    std::string i3, i4, i5;
+    std::cout << "Cuál es tu edad?\n";
+    std::cin >> i1;
+    std::cout << "Cuál es tu número\n";
+    std::cin >> i2;
+    std::cout << "Cual es tu nombre?\n";
+    std::cin >> i3;
+    std::cout << "Tu direccion es...\n";
+    std::cin >> i4;
+    std::cout << "Cual es tu hobby?\n";
+    std::cin >> i5;
+    std::cout << std::endl;
+    return 0;
+ 
+
+    float numero1, numero2;
+    std::cout << "Primer numero: \n";
+    std::cin >> numero1;
+    std::cout << "Segungdo numero: \n";
+    std::cin >> numero2;
+    std::cout << "La suma es "<<numero1 + numero2;
+    std::cout << std::endl;
+    std::cout << "La resta es: "<<numero1 - numero2;
+    std::cout << std::endl;
+    std::cout << "La multiplicacion es: "<<numero1 * numero2;
+    std::cout << std::endl;
+    std::cout << "La division es: "<<numero1 / numero2;
+    std::cout << std::endl;
+    std::cout << "raiz de "<<numero1<<" es "<<sqrt(numero1);
+    std::cout << std::endl;
+    std::cout << "raiz de "<<numero2<<" es "<<sqrt(numero2);
+    std::cout << std::endl;
+    return 0;
+    */
+
+    float b, h, a;
+    std::cout << "base del triangulo:\n";
+    std::cin >> b;
+    std::cout << "altura del triangulo:\n";
+    std::cin >> h;
+    a = (b * h) / 2;
+    std::cout << "el area del triangulo es: " << a << std::endl;
+
+    float pi, r, a2;
+    std::cout << "radio del circulo: \n";
+    std::cin >> r;
+    pi = 3, 14;
+    a2 = pi * pow(r, 2);
+    std::cout << "el area del circulo es: " << a2 << std::endl;
+
+    float l, a3;
+    std::cout << "lado del cuadrado: \n";
+    std::cin >> l;
+    a3 = pow(l, 2);
+    std::cout << "el area del cuadrado es: " << a3 << std::endl;
+
+}
